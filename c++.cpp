401a7fcf0b856a2749cfd19c8afc7fc375c54de5
Alex Wall <alex@noligy.de>
2011-06-29 20:37:06 +0200

    for each

--- c++.cpp
+++ c++.cpp
@@ -7,3 +7,16 @@ enum Phases {
 class Test{
 	Test();
 }
+
+/* BOOST_FOREACH */
+BOOST_FOREACH(SetVector3D::value_type const & tempVec, all_my_vectors){
+	// do something with tempVec
+}
+
+/* Iterator for each */
+for (unordered_map::iterator it = map.begin(); it != map.end(); ++it) {
+	 // key
+	 	cout << it->first;
+	 // value
+		cout << it->second;
+}
